c3a555b9d78d2ec02fdd1700a9641891d494edd6
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-06-20 13:49:58 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -1,87 +1,70 @@
-const prompt = require("prompt-sync")();
-let a = Number(prompt("enter how many time will prites = "));
-//square pattarn
-for(let i = 1;i<=a;i++){
-    for(let j=1;j<=a;j++){
-    process.stdout.write("* ")
+const prompt = require('prompt-sync')();
+let n = Number(prompt('enter a number = '));
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write('*');
+    }
+    console.log()
+}
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        process.stdout.write('* ');
     }
     console.log();
 }
-// right angle patter
-for(let i = 1; i<=a;i++){
-    for(let j=1;j<=i;j++){
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(j + " ");
+    }
+    console.log();
+}
+
+for (let i = 1; i <= n; i++) {
+    let ch = 65;
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(String.fromCharCode(ch) + " ");
+        ch++
+    }
+    console.log()
+}
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i + 1; j++) {
+        process.stdout.write('*')
+    }
+    console.log()
+}
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i + 1; j++) {
+        process.stdout.write(" ")
+    }
+    for (let j = 1; j <= i; j++) {
         process.stdout.write("* ")
     }
     console.log()
 }
-// 
-for(let i=1;i<=a;i++){
-    let p = 1;
-    for(let j=1;j<=i;j++){
-        process.stdout.write(p +" ")//process.stdout.write it's pass only string value
-        p++;
-    }
-    console.log()
-}
-// 
-for(let i=1;i<=a;i++){
-    let ascii = 65;
-    for(let j=1;j<=i;j++){
-        process.stdout.write(String.fromCharCode(ascii) + " ");
-        ascii++;
-    }
-    console.log()
-}
-// 
-for(let i = 0;i<=a;i++){
-    for(let j=1;j<=a-i;j++){
-        process.stdout.write('* ')
-    }
-    console.log()
-}
-// 
-for(let i=1;i<=a;i++){
-    //inverted
-    for(let j=1;j<=a-i+1;j++){
-        process.stdout.write(' ')
-    }
-    //right angel
-    for(let j=1;j<=i;j++){
-        process.stdout.write('* ')
-    }
-    console.log();
-}
-// 
-for(let i=1;i<=a;i++){
-    //inverted
-    for(let j=1;j<=a-i+1;j++){
-        process.stdout.write('  ')
-    }
-    //right angel
-    for(let j=1;j<=i;j++){
-        process.stdout.write('* ')
-    }
-    console.log();
-}
-// X
-for(let i=1;i<=a;i++){
-    for(let j=1;j<=a;j++){
-        if(i==j || i+j == a+1){
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        if (i == j || i + j == n + 1) {
             process.stdout.write("* ")
-        }else{
+        } else {
             process.stdout.write("  ")
         }
     }
     console.log()
 }
-// y patter
-for(let i=1;i<=a;i++){
-    for(let j=1;j<=2*a-1;j++){
-        if(i == j || j+i == 2*a){
-            process.stdout.write("* ")
-        }else{
-            process.stdout.write("  ");
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= 2 * n - 1; j++) {
+        if (i == j || i + j == 2 * n) {
+            process.stdout.write("*")
+        } else {
+            process.stdout.write('  ')
         }
     }
-    console.log();
+    console.log()
 }
